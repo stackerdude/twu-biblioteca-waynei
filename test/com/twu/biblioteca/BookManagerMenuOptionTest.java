b23cfe0d7008ae422c97fa60bd7f48835ceae86b
stackerdude <wayne.j.irwin@gmail.com>
2018-05-03 21:40:12 +1000

    Adding Support For Main Menu

---
+++ test/com/twu/biblioteca/BookManagerMenuOptionTest.java
@@ -0,0 +1,17 @@
+package com.twu.biblioteca;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+
+
+public class BookManagerMenuOptionTest {
+
+    @Test
+    public void TestPrintsBookListWhenMenuOptionPressed{
+
+    }
+}
